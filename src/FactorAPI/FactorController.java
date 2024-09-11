74e793c462216aa3337d6274e5526ecf2e425d26
Oliver Dvorak <odvorak04@gmail.com>
2024-09-10 20:55:29 -0400

    This is API 1, if you are doing API 2, its important to look through as there are some parts intertwined with API 1. Some interfaces here are part of API 2 and need to be implemented.

---
+++ src/FactorAPI/FactorController.java
@@ -0,0 +1,5 @@
+package FactorAPI;
+//API 2 (Needs Implementation)
+public interface FactorController {
+    GetFactorResponse doFactor(GetFactorRequest factorRequest);
+}
---
+++ src/FactorAPI/LoginController.java
@@ -0,0 +1,10 @@
+package FactorAPI;
+
+public interface LoginController {
+
+	/** Logs the user in
+	 * @param loginRequest - contains the info needed to log a user in
+	 * */
+	LoginResponse login(LoginRequest loginRequest);
+
+}
