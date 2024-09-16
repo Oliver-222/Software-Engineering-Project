1cd9d6b151a3dd9c719b7a5e9b9c22b01315c049
mirahsupreme <alstonamirah@gmail.com>
2024-09-15 23:38:53 -0400

    Create ComputeJobRequest.java

---
+++ src/FactorAPI/package factorapi; /ComputeJobRequest.java
@@ -0,0 +1,10 @@
+package factorapi;
+
+public interface ComputeJobRequest {
+    String getSource();
+    String getDelimiter();
+    String getDestination();
+    String getComputationType();
+}
+
+  
