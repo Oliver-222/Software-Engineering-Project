e71c27e2d89e60305747e040a902eda088398d71
mirahsupreme <alstonamirah@gmail.com>
2024-09-16 01:29:03 -0400

    Update ComputeEngine.java

--- src/FactorAPI/ComputeEngine.java
+++ src/FactorAPI/ComputeEngine.java
@@ -1,25 +1,8 @@
 package factorapi;
 
-import java.util.Base64;
+import otherpackage.ComputeJobRequest;
 
-
-/**
- * Interface for the compute engine.
- */
 public interface ComputeEngine {
-    /**
-     * Initializes a job with the given request.
-     *
-     * @param request The job request.
-     * @return A string indicating the status of initialization.
-     */
     String initializeJob(ComputeJobRequest request);
-
-    /**
-     * Executes the job with the given request.
-     *
-     * @param request The job request.
-     * @return A string indicating the status of execution.
-     */
     String executeJob(ComputeJobRequest request);
 }
