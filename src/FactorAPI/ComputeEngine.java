d4428fc5807c6985928a75e00700438bc9d7d248
mirahsupreme <alstonamirah@gmail.com>
2024-09-16 00:09:42 -0400

    Create ComputeEngine.java

---
+++ src/FactorAPI/ComputeEngine.java
@@ -0,0 +1,21 @@
+package factorapi;
+
+/**
+ * Interface for the compute engine operations.
+ */
+public interface ComputeEngine {
+
+    /**
+     * Initializes a computation job.
+     * @param request Compute job request.
+     * @return Status of initialization.
+     */
+    String initializeJob(ComputeJobRequest request);
+
+    /**
+     * Executes the computation.
+     * @param request Compute job request.
+     * @return Result of the computation.
+     */
+    String executeJob(ComputeJobRequest request);
+}
