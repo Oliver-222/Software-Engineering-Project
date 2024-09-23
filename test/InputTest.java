98aff779a4bc086241df0e3e19b04a1d092b343d
loganullmeyer <logan.ullmeyer@gmail.com>
2024-09-23 00:56:38 -0400

    Update InputTest.java
    Adding more constructors

--- test/InputTest.java
+++ test/InputTest.java
@@ -4,6 +4,13 @@ import java.util.List;
 
 public class InputTest implements ReadResponse{
 	//Test-only input implementation
+	public InputTest(){
+		this(null);
+	}
+	public InputTest(List<Integer> data){
+		this.data = data; 
+	}
+	
 	public List<Integer> getData() {
 		return this.data; 
 	}
