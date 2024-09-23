a554180bee245d1c9de81b5d18fb48bdea7923b6
mirahsupreme <alstonamirah@gmail.com>
2024-09-23 00:36:11 -0400

    Update ComputeEngineTest.java
    fixed mockito

--- src/FactorAPI/ComputeEngineTest.java
+++ src/FactorAPI/ComputeEngineTest.java
@@ -1,7 +1,8 @@
 package factorapi;
 
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ComputeEngineTest {
     @Test
