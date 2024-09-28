47e59525d30949593a5b7581d5b6d30111a13a4d
loganullmeyer <logan.ullmeyer@gmail.com>
2024-09-28 01:21:30 -0400

    Update ReadResponse.java
    Removed the final data List, since it needs to be updated as the data is read. Added a getData method in case it's needed

--- src/FactorAPI/ReadResponse.java
+++ src/FactorAPI/ReadResponse.java
@@ -3,7 +3,10 @@ package factorapi;
 import java.util.List; 
 
 public interface ReadResponse {
+	//Getting rid of this because it needs to be updated once it has the data
+	//public static List<Integer> data = null;
 
-	public static List<Integer> data = null;
+	//I figure this would probably be needed at some point, couldn't hurt to have
+	public List<Integer> getData();
 	
 }
