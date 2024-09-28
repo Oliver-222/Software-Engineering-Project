751f1640849f052674a10ab18de8bcf1e2856cb9
loganullmeyer <logan.ullmeyer@gmail.com>
2024-09-28 01:19:22 -0400

    Update DataStoreImpl.java
    Changed this empty implementation to the one we need for part 3 of the assignment. This will read/write user-specified files

--- src/FactorAPI/DataStoreImpl.java
+++ src/FactorAPI/DataStoreImpl.java
@@ -1,18 +1,63 @@
 package factorapi;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class DataStoreImpl implements DataStorage{
 
 	
 	@Override
-	public ReadResponse read(ReadRequest readRequest) {
+	public ReadResponse read(ReadRequest readRequest) throws FileNotFoundException {
 		
-		return null;
+		//File object for the specified source file name. Throws exception if the file doesn't exist.
+		File readFile = new File(readRequest.source);
+		
+		//To read the file, implements Iterable, so can handle arbitrarily large inputs
+		Scanner sc = new Scanner(readFile);
+		
+		//The actual integers being read from the file. This will be given to the ReadResponse as its data field
+		ArrayList<Integer> readData = new ArrayList<>();
+		
+		
+		//While the scanner still finds integers in the source file, read them into the data List
+		while(sc.hasNextInt()) {
+			readData.add(sc.nextInt());
+		}
+		
+		//Lets us return the ReadResponse. Only contains the data List
+		DataReadResponse dRR = new DataReadResponse(readData);
+		
+		return dRR;
+
 	}
 
 	@Override
-	public WriteResponse write(WriteRequest writeRequest) {
+	public WriteResponse write(WriteRequest writeRequest) throws IOException {
+		//Make a new file instance to be called by next line
+		File writeFile = new File(writeRequest.destination);
+		//If the file exists, does nothing. if not, creates a new file with the specified name. 
+		//Returns a boolean that I don't care about, at least for right now. 
+		writeFile.createNewFile();
 		
-		return null;
+		//Writes things to the specified file 
+		FileWriter fw = new FileWriter(writeFile);
+		
+		//To iterate over potentially large amounts of data from the compute engine. 
+		Iterator<Integer> writeIterator = writeRequest.writeData.iterator();
+		
+		while(writeIterator.hasNext()) {
+			fw.write(writeIterator.next());
+		}
+		
+		WriteResponse wR = new DataWriteResponse();
+		
+		return wR;
 	}
 
 }
