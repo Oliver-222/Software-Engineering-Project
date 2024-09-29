820e3b81d2f4dda681a04084de42d74e1200fa81
Oliver Dvorak <odvorak04@gmail.com>
2024-09-29 15:01:33 -0400

    Created TestFactorAPI Test

---
+++ test/TestFactorAPI.java
@@ -0,0 +1,28 @@
+import factorapi.FactorAPI;
+import factorapi.GetFactorRequestImpl;
+import factorapi.GetFactorResponse;
+import factorapi.GetFactorResponseImpl;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class TestFactorAPI {
+    @Test
+    public void TestProcessRequestReturnsSuccess(){
+        ArrayList<Integer> factors = new ArrayList<>();
+        factors.add(4);
+        factors.add(6);
+        String delimiter = ":";
+        String source = "source";
+        String destination = "destination";
+
+
+        FactorAPI factorAPI = new FactorAPI();
+        GetFactorRequestImpl request = new GetFactorRequestImpl(factors,delimiter,source,destination);
+
+        GetFactorResponse response = factorAPI.processRequest(request);
+
+        assertEquals(GetFactorResponse.GetFactorStatus.SUCCESS, response.getStatus());
+    }
+}
