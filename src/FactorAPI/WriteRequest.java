988ae5408798fdeb12b9304e196c5cda7d68abc6
loganullmeyer <logan.ullmeyer@gmail.com>
2024-09-30 09:42:28 -0400

    New constructor that doesn't need data

--- src/FactorAPI/WriteRequest.java
+++ src/FactorAPI/WriteRequest.java
@@ -1,5 +1,8 @@
 package factorapi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WriteRequest {
 
 	//Will contain the result from the compute engine and the desired output location 
@@ -9,6 +12,11 @@ public class WriteRequest {
 
 	//Will have to be passed the List of data to write to the desired file.
 	//This will be the result of the factor, probably sent by compute engine.
+	public WriteRequest(GetFactorRequest factorRequest){
+		this.destination = factorRequest.getDestination();
+		this.writeData = new ArrayList<Integer>();
+	}
+	
 	public WriteRequest(GetFactorRequest factorRequest, List<Integer> writeData) {
 		this.destination = factorRequest.getDestination();
 		this.writeData = writeData; 
