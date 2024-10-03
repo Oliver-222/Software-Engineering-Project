92e0798bb769cb672beb9b056cbe9e06e8fdd2b6
Oliver Dvorak <odvorak04@gmail.com>
2024-10-02 22:11:06 -0400

    no message

--- test/ComputeEngineTest.java
+++ test/ComputeEngineTest.java
@@ -7,15 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ComputeEngineTest {
     @Test
-    void testInitializeJob() {
-        ComputeRequest mockRequest = mock(ComputeRequest.class);
-        ComputeEngine computeEngine = new ComputeEngineImpl(mockRequest);
-
-        String response = computeEngine.executeJob(mockRequest);
-        assertEquals("", response); // Test should fail since the method returns an empty string
-    }
-
-    @Test
     void testExecuteJob() {
         ComputeRequest mockRequest = mock(ComputeRequest.class);
         ComputeEngine computeEngine = new ComputeEngineImpl(mockRequest);
