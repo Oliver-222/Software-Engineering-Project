07178913a3a7ced081611022ba30d85101a019d6
loganullmeyer <logan.ullmeyer@gmail.com>
2024-10-06 23:06:11 -0400

    Update TestGetFactorResponse.java exceptions

--- test/TestGetFactorResponse.java
+++ test/TestGetFactorResponse.java
@@ -6,9 +6,10 @@ import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 public class TestGetFactorResponse {
       @Test
-      public void testSuccessfulResponse() throws FileNotFoundException{
+      public void testSuccessfulResponse() throws FileNotFoundException, IOException{
         FactorAPI mockApi = Mockito.mock(FactorAPI.class);
         GetFactorRequest mockReq = Mockito.mock(GetFactorRequest.class);
         GetFactorResponse mockResp = Mockito.mock(GetFactorResponse.class);
