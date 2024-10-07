6dd58051c7e69be788296eae3b25db764ef35fbc
loganullmeyer <logan.ullmeyer@gmail.com>
2024-10-06 22:54:15 -0400

    Update FactorAPI.java

--- src/FactorAPI/FactorAPI.java
+++ src/FactorAPI/FactorAPI.java
@@ -1,6 +1,32 @@
 package factorapi;
+
+import java.io.FileNotFoundException;
+
 public class FactorAPI {
-    public GetFactorResponse processRequest(GetFactorRequest factorRequest) {
-        return new GetFactorResponseImpl(GetFactorResponse.GetFactorStatus.SUCCESS);
-    }
+    public GetFactorResponse processRequest(GetFactorRequest factorRequest) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+		
+		//Holds the source information
+		ReadRequest readRequest = new ReadRequest(factorRequest);
+		
+		//Make a DataStorage object to read the desired input data
+		DataStorage ds = new DataStoreImpl();
+		DataReadResponse readResponse = (DataReadResponse) ds.read(readRequest);
+		
+		//Necessary objects before the computation
+		ComputeRequest computeRequest = new ComputeRequest((ArrayList<Integer>) readResponse.getData(), factorRequest.getSource(), factorRequest.getDestination());
+		ComputeEngine computeEngine = new ComputeEngineImpl(computeRequest);
+		
+		//Make the engine find the result of our input
+		String data = computeEngine.executeJob(computeRequest);
+		
+		//Object the DataStore can use
+		WriteRequest writeRequest = new WriteRequest(factorRequest, data);
+		//Make the DataStore write the result to the desired destination 
+		WriteResponse writeResponse = ds.write(writeRequest);
+		
+		//Tell the user that it wrote successfully 
+		return new GetFactorResponseImpl(GetFactorResponse.GetFactorStatus.SUCCESS);
+		
+	}
 }
