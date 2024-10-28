b3f20cb33e6d77d917b6eb5acfbcf9d9b1c93217
loganullmeyer <logan.ullmeyer@gmail.com>
2024-10-28 00:37:16 -0400

    Update CoordinatorImpl.java imports

--- src/FactorAPI/CoordinatorImpl.java
+++ src/FactorAPI/CoordinatorImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors; 
+import java.util.concurrent.Callable; 
 
 public class CoordinatorImpl implements ComputationCoordinator {
 
