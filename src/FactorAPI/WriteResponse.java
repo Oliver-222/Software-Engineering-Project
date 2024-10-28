d55ec2227e6a63fb7a6a355b8aae637a140be0bc
loganullmeyer <logan.ullmeyer@gmail.com>
2024-10-27 23:47:44 -0400

    Update WriteResponse.java
    Add default constructor

--- src/FactorAPI/WriteResponse.java
+++ src/FactorAPI/WriteResponse.java
@@ -4,6 +4,10 @@ public class WriteResponse {
 
     private WriteResponseStatus status;
 
+    public WriteResponse(){
+        this(WriteResponseStatus.SUCCESS);
+    }
+    
     public WriteResponse(WriteResponseStatus status){
         this.status = status; 
     }    
